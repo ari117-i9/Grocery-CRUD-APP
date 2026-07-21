501c3fb38dcb36eedcb443c2e86e6290648fd9b9
ari <ari.imran.f@gmail.com>
2026-07-21 16:08:17 +0800

    create route to save a product to database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
 
+app.use(express.json)
+
 //routes
-app.get('/', (req, res)=>{
+app.get('/', async(req, res)=>{
     res.send('hello node api, whats up, yo sup')
 })
 
-
+app.post('/product', (req, res)=>{
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
 
 
 mongoose.
