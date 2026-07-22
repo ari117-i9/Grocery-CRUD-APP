05731da0339abd209e150f151086529695c08533
ari <ari.imran.f@gmail.com>
2026-07-22 16:59:16 +0800

    update middleware to accept url form

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const Product = require('./models/productModel')
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 //routes
 app.get('/inventory', async(req, res)=>{
