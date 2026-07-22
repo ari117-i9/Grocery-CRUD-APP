8ada885384d90282c8185f94870d878c9f8db356
ari <ari.imran.f@gmail.com>
2026-07-22 15:54:04 +0800

    retrieve all data or individually by id from database

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,35 @@
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '1.1.1.1'])
 const express = require('express')
 const mongoose = require('mongoose')
 const Product = require('./models/productModel')
 const app = express()
 
-app.use(express.json)
+app.use(express.json())
 
 //routes
-app.get('/', async(req, res)=>{
-    res.send('hello node api, whats up, yo sup')
+app.get('/inventory', async(req, res)=>{
+    try {
+        const products = await Product.find({})
+        res.status(200).json(products)  
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
 })
 
-app.post('/product', (req, res)=>{
+app.get('/inventory/:id', async(req, res)=>{
+    try {
+        const {id} = req.params
+        const products = await Product.findById(id)
+        res.status(200).json(products)  
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        
+    }
+})
+
+app.post('/inventory', async(req, res)=>{
     try {
         const product = await Product.create(req.body)
         res.status(200).json(product);
@@ -22,7 +41,7 @@ app.post('/product', (req, res)=>{
 
 
 mongoose.
-connect('mongodb+srv://admin:%40dmin4321@cluster0.c23zfgi.mongodb.net/?appName=Cluster0')
+connect('mongodb+srv://admin:%40dmin4321@cluster0.c23zfgi.mongodb.net/Inventory?appName=Cluster0')
 .then(()=>{
     console.log('connected to mongoDB')
     app.listen(3000, ()=>{
