0328de8a55a910e74bff426a49470b9d70835311
ari <ari.imran.f@gmail.com>
2026-07-23 17:55:11 +0800

    create pages for crud app

--- frontend/inventory-crud/src/App.jsx
+++ frontend/inventory-crud/src/App.jsx
@@ -1,9 +1,11 @@
 const App = () => {
     return(
         <div>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+            <Routes>
+                <Route index element={}>
+
+                </Route>
+            </Routes>
         </div>
     );
 }
