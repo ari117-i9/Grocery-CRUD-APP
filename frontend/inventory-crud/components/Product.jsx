372dc52d56d06eaa25267f4069e8da6accf23591
ari <ari.imran.f@gmail.com>
2026-07-23 21:16:38 +0800

    implement product component

---
+++ frontend/inventory-crud/components/Product.jsx
@@ -0,0 +1,21 @@
+import {Link} from "react-router-dom"
+const Product = ({product}) => {
+    return (
+        <div className="bg-white rounded shadow-lg overflow-hidden">
+            <img src={product.image} className="w-fullwidth h-28 object-cover"/>
+            <div className="px-4 pt-2 pb-4">
+                <h2 className="text font-semibold">{product.name}</h2>
+                <div className="test-sm">Quantity: {product.quantity}</div>
+                <div className="test-sm">Price: ${product.price}</div>
+                <div className="mt-2 flex gap-4 ">
+                     <Link to={`/edit`} className="inline-block w-full text-center shadow-md text-sm bg-gray-700 text-white rounded-sm px-4 py-1 font-bold hover:bg-gray-600 hover:cursor-pointer">
+                     Edit</Link>
+                     <Link to={`/edit`} className="inline-block w-full text-center shadow-md text-sm bg-red-700 text-white rounded-sm px-4 py-1 font-bold hover:bg-red-600 hover:cursor-pointer">
+                     Delete</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Product
