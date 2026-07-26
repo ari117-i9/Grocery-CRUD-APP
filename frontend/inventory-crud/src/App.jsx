b3bd02d48f440339e3da1b63acaff99d3539f076
ari <ari.imran.f@gmail.com>
2026-07-27 02:20:03 +0800

    implement all pages with css, change product component

--- frontend/inventory-crud/src/App.jsx
+++ frontend/inventory-crud/src/App.jsx
@@ -1,11 +1,29 @@
+import { Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Add from './pages/Add'
+import Add from './pages/Update'
+import{ ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
+export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 const App = () => {
     return(
         <div>
-            <Routes>
-                <Route index element={}>
+            <nav className='bg-gray-800'>
+                <div className='container mx-auto p-2'>
+                    <Link to="/"><h2 className='text-white text-2xl font-bold'>React CRUD</h2></Link>
+                </div>
+            </nav>
 
-                </Route>
-            </Routes>
+            <div>
+                <Routes className="container mx-auto p-2 h-full">
+                    <Route index element={<Home/>}></Route>
+                    <Route path='/add' element={<Add/>}></Route>
+                    <Route path='/update/:id' element={<Update/>}></Route>
+                </Routes>
+            </div>
+            <ToastContainer/>
+            
         </div>
     );
 }
